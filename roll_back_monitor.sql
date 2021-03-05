3f00d072fa48891599c5ec79fccb87028f61b292
comput3 <74689393+comput3@users.noreply.github.com>
2021-03-05 00:22:15 -0600

    Create roll_back_monitor.sql

---
+++ roll_back_monitor.sql
@@ -0,0 +1,21 @@
+SET LINESIZE 300
+COLUMN username FORMAT A20
+COLUMN sid FORMAT 9999
+COLUMN serial# FORMAT 99999
+SELECT s.username,
+       s.sid,
+       s.serial#,
+       t.used_ublk,
+       t.used_urec,
+       rs.segment_name,
+       r.rssize,
+       r.status
+FROM   gv$transaction t,
+       gv$session s,
+       gv$rollstat r,
+       dba_rollback_segs rs
+WHERE  s.saddr = t.ses_addr
+AND    t.xidusn = r.usn
+AND   rs.segment_id = t.xidusn
+ORDER BY t.used_ublk DESC
+/
